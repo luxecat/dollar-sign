87fea179d24b55f5d2ac42a93727b9068ed4bd78
Nishith Munasinghe <luxecat3@gmail.com>
2024-12-01 19:52:30 +1100

    Do some code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-const hello: string = "world";
+#!/usr/bin/env node
+
+import { spawn } from "node:child_process";
+
+const args = process.argv.slice(2);
+
+if (!args[0]) {
+  throw new Error("Please provide a command to run");
+}
+
+const cmd = spawn(args[0], args.slice(1), {
+  stdio: "inherit",
+});
+
+cmd.on("exit", (code) => {
+  process.exit(code);
+});
